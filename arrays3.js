315e5474309bcb3fe223de8b80404e37487525ea
Anton <waytofall@inbox.ru>
2021-04-30 22:08:47 +0300

    arrays

--- arrays3.js
+++ arrays3.js
@@ -1,144 +1,3 @@
-// //+ #115  Row Weights
-
-// function rowWeights(array) {
-//   let firstTeam = 0
-//   let secondTeam = 0
-//   for (let i = 0; i < array.length; i++) {
-//     if (i % 2 === 0) {
-//       firstTeam += array[i]
-//     } else {
-//       secondTeam += array[i]
-//     }
-//   }
-//   return [firstTeam, secondTeam]
-// }
-
-// console.log(rowWeights([80])) //, [80, 0])
-// console.log(rowWeights([100, 50])) //, [100, 50])
-// console.log(rowWeights([0])) //, [0, 0])
-// console.log(rowWeights([39, 84, 74, 18, 59, 72, 35, 61])) //, [207, 235])
-// console.log(rowWeights([0, 1, 0])) //, [0, 1])
-
-// //+ #116  Scrolling Text
-
-// function scrollingText(text) {
-//   let newText = text.toUpperCase()
-//   const array = []
-//   for (let i = 0; i < newText.length; i++) {
-//     array.push(newText)
-//     newText = newText.slice(1) + newText[0]
-//   }
-//   return array
-// }
-
-// console.log(scrollingText('abc')) //, ['ABC', 'BCA', 'CAB'])
-
-//+ #117 zipWith
-
-// function zipWith(fn, a0, a1) {
-// // return a0.slice(0, a1.length).map((_, i) => fn(a0[i], a1[i]))
-// const arr = []
-// const len = Math.min(a0.length, a1.length)
-// for (let i = 0; i < len; i++) {
-//   console.log(a0[i], a1[i])
-//   arr.push(fn(a0[i], a1[i]))
-// }
-// return arr
-// }
-// const plus = (a, b) => a + b
-
-// console.log(zipWith(plus, [0, 1, 2, 3, 4, 5], [6, 5, 4, 3, 2, 1])) //, [6,6,6,6,6,6]);
-// console.log(zipWith(plus, [0, 1, 2, 3, 4], [6, 5, 4, 3, 2, 1])) //, [6,6,6,6,6  ]);
-// console.log(zipWith(plus, [0, 1, 2, 3, 4, 5], [6, 5, 4, 3, 2])) //, [6,6,6,6,6  ]);
-
-// //+ #118 Beginner - Lost Without a Map
-// function maps(x) {
-//   return x.map((num) => num * 2)
-// }
-
-// console.log(maps([1, 2, 3])) //, [2, 4, 6]);
-
-// //+ #119 Array plus array
-// function arrayPlusArray(arr1, arr2) {
-//   return [...arr1, ...arr2].reduce((a, b) => a + b, 0)
-// }
-
-// console.log(arrayPlusArray([], [])) //, 0);
-// console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])) //, 21);
-// console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])) //, -21);
-
-// //+ #120  Is every value in the array an array?
-
-// function arrCheck(value) {
-//   return value.every((item) => Array.isArray(item))
-// }
-// console.log(arrCheck([])) //, true);
-// console.log(arrCheck(['A', 'R', 'R', 'A', 'Y'])) //, false);
-
-// // [...arr1, ...arr2].every(f) === arr1.every(f) && arr2.every(f)
-
-// // arr2.every(f) === [].every(f) && arr2.every(f)
-
-// // https://en.wikipedia.org/wiki/Vacuous_truth
-
-// //+ #121  Make a square box!
-
-// function box(n) {
-//   const arr = []
-//   arr.push('-'.repeat(n))
-//   for (let i = 0; i < n - 2; i++) {
-//     arr.push('-' + ' '.repeat(n - 2) + '-')
-//   }
-//   arr.push('-'.repeat(n))
-//   return arr
-// }
-
-// console.log(box(5)) //, ['-----', '-   -', '-   -', '-   -', '-----'])
-// console.log(box(2)) //, ['--', '--'])
-// console.log(box(3)) //, ['---', '- -', '---'])
-
-//+ #122  Magic Index
-// function findMagic(arr) {
-//   //return arr.find((item,ind) => item === ind) || -1;
-//   //return arr.findIndex((ind) => arr[ind] === ind)
-//   let magicNum = -1
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === i) {
-//       magicNum = arr[i]
-//     }
-//   }
-//   return magicNum
-// }
-
-// // //+ #123 Flatten and sort an array
-// function flattenAndSort(array) {
-//   /* const newArray = array.reduce((a, b) => a.concat(b), [])
-//     return newArray.sort((a, b) => a - b) */
-//   return [].concat(...array).sort((a, b) => a - b)
-// }
-// console.log(flattenAndSort([])) //, []);
-// console.log(flattenAndSort([[], []])) //, []);
-// console.log(flattenAndSort([[], [1]])) //, [1]);
-// console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])) //, [1, 2, 3, 4, 5, 6, 100]);
-
-// //+ #124 Two Sum
-
-// function twoSum(numbers, target) {
-//   for (let i = 0; i < numbers.length; i++) {
-//     for (let j = i + 1; j < numbers.length; j++) {
-//       if (numbers[i] + numbers[j] === target) {
-//         return [i, j]
-//       }
-//     }
-//   }
-// }
-
-// console.log(twoSum([9, 2, 1, 3], 4)) // [2,3]
-
-// console.log(twoSum([1, 2, 3], 4)) //.sort(numericalCompare)) //, [0,2]);
-// console.log(twoSum([1234, 5678, 9012], 14690)) //.sort(numericalCompare)) //, [1,2]);
-// console.log(twoSum([2, 2, 3], 4)) //.sort(numericalCompare)) //, [0,1]);
-
 // // #125  Length of missing array ------ надо дорешать
 // function getLengthOfMissingArray(arrayOfArrays) {
 //   const s = arrayOfArrays.map((array) => array.length)
@@ -172,21 +31,6 @@
 //     ['a', 'a', 'a', 'a', 'a', 'a'],
 //   ])
 // ) //, 5);
-// //+ #126  Data Reverse
-// function dataReverse(data) {
-//   const reverse = []
-//   for (let i = 0; i < data.length; i += 8) {
-//     let a = data.slice(i, i + 8)
-//     reverse.unshift(...a)
-//   }
-//   return reverse
-// }
-
-// //const data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
-// console.log(dataReverse(data1)) //,data2)
-// const data3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]
-// //const data4 = [0,0,1,0,1,0,0,1,0,0,1,1,0,1,1,0]
-// console.log(dataReverse(data3)) //,data4)
 
 //+ #127  Duplicate Encoder  ---- не проходит тесты
 // function duplicateEncode(word) {
@@ -213,29 +57,6 @@
 // console.log(duplicateEncode('Success')) //,")())())","should ignore case");
 // console.log(duplicateEncode('(( @')) //,"))((");
 
-//+ #128 Alternate capitalization
-
-// function capitalize(s) {
-//   arrLetters = s.split('')
-
-//   let firstCap = ''
-//   let secondCap = ''
-
-//   for (let i = 0; i < arrLetters.length; i++) {
-//     if (i % 2 === 0) {
-//       firstCap += arrLetters[i].toUpperCase()
-//       secondCap += arrLetters[i]
-//     } else {
-//       firstCap += arrLetters[i]
-//       secondCap += arrLetters[i].toUpperCase()
-//     }
-//   }
-//   return [firstCap, secondCap]
-// }
-
-// console.log(capitalize('abracadabra')) //, ['AbRaCaDaBrA', 'aBrAcAdAbRa'])
-// console.log(capitalize('codewarriors')) //, ['CoDeWaRrIoRs', 'cOdEwArRiOrS'])
-
 // // #129 Sort the odd ----
 
 // function sortArray(array) {
@@ -246,40 +67,7 @@
 // console.log(sortArray([5, 3, 1, 8, 0])) //, [1, 3, 5, 8, 0]);
 // console.log(sortArray([])) //,[]);
 
-// ////+ №130   Format a string of names like 'Bart, Lisa & Maggie'.
-// function list(characters) {
-//   const names = characters.map((x) => x.name)
-
-//   if (names.length <= 1) {
-//     return names.join('')
-//   }
-
-//   const str1 = names.slice(0, -1)
-//   return `${str1.join(', ')} & ${names[names.lenght - 1]}`
-// }
-
-// // console.log(list([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }])) ///, 'Bart, Lisa & Maggie',
-// // //"Must work with many names")
-// // console.log(list([{ name: 'Bart' }, { name: 'Lisa' }])) //, 'Bart & Lisa',
-// // //"Must work with two names")
-// // console.log(list([{ name: 'Bart' }])) ///   , 'Bart', "Wrong output for a single name")
-// // console.log(list([])) ///, '', "Must work with no names")
-
-// //+ #131
-// function proofread(str) {
-//   return str
-//     .toLowerCase()
-//     .replace(/ie/g, 'ei')
-//     .split('. ')
-//     .map((newArr) => newArr[0].toUpperCase() + newArr.slice(1))
-//     .join('. ')
-// }
-// console.log(proofread('ThiEr DEcIEt wAs INconcIEVablE. sHe SIeZeD thE moMENT.')) //, "Their deceit was inconceivable. She seized the moment.");
-// console.log(proofread('HIs nieghBOur wAs oVerwieGht.')) //, "His neighbour was overweight.");
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // // #132 Simple Fun #79: Delete a Digit  ----
-
 // function deleteDigit(n) {
 //   //coding and coding..
 // }
