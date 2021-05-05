34c48b88e613d9b9b90acaaffb8f83aab07596d3
Anton <waytofall@inbox.ru>
2021-05-05 07:59:49 +0300

    arrays

--- arrays4.js
+++ arrays4.js
@@ -1,39 +1,8 @@
-// //+ #125  Length of missing array  сделал проверку на null
-
-// const isNullOrEmpty = (value) => value === null || value.length === 0
-
-// function getLengthOfMissingArray(arrayOfArrays) {
-//   if (isNullOrEmpty(arrayOfArrays) || arrayOfArrays.some(isNullOrEmpty)) {
-//     return 0
-//   }
-
-//   const s = arrayOfArrays.map((array) => array.length).sort((a, b) => a - b)
-
-//   for (let i = 0; i < s.length - 1; i++) {
-//     if (s[i + 1] - s[i] !== 1) {
-//       return s[i] + 1
-//     }
-//   }
-// }
-// console.log(
-//   getLengthOfMissingArray([
-//     [],
-//     [0, 2],
-//     [2],
-//     [3, 4, 3, 0],
-//     [0, 3, 1, 4, 4],
-//     [0, 2, 4, 3, 2, 2],
-//   ])
-// ) //, 3);
-// console.log(
-//   getLengthOfMissingArray([[5, 2, 9], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]])
-// ) //, 2);
-// console.log(getLengthOfMissingArray([])) //, 2);
-
-// #129 Sort the odd ----
+// //+ #129 Sort the odd ???
 
 // function sortArray(array) {
-//   const odds = array.filter((num) => num % 2).sort((a, b) => a - b)
+//   const odds = array.filter((num) => num % 2).sort((a, b) => a - b) //////// (num % 2 === 1) НЕ проходит тесты с отрицательными числами.
+//   console.log('...', odds)
 //   let i = 0
 //   const newArr = array.map((item) => {
 //     if (item % 2 === 0) {
@@ -53,27 +22,39 @@
 // console.log(sortArray([5, 3, 1, 8, 0])) //, [1, 3, 5, 8, 0]);
 // console.log(sortArray([])) //,[]);
 
-// //#132 Simple Fun #79: Delete a Digit  ----
+// //+#132 Simple Fun #79: Delete a Digit
 // function deleteDigit(n) {
-//   let arr = Array.from(n.toString())
-//   const result = []
+//   const arr = Array.from(n.toString())
+//   let number = 0
 //   for (let i = 0; i < arr.length; i++) {
-
+//   let sum = +(arr.slice(0, i) + arr.slice(i + 1)).split(',').join('')
+//     if (sum > number) {
+//       number = sum
+//     }
 //   }
-//   return result.join('')
+//   return number
 // }
 
 // console.log('>>>', deleteDigit(1234567))
-// // console.log(deleteDigit(152)) //,52)
+// console.log(deleteDigit(152)) //,52)
 
 // console.log(deleteDigit(1001)) //,101)
 
-// console.log(deleteDigit(10)) //,1)
+//  console.log(deleteDigit(10)) //,1)
 
-// #133  Equal Sides Of An Array  ----
+//+ #133  Equal Sides Of An Array
 // function findEvenIndex(arr) {
-//   for (let i = 1; i < arr.length - 1; i++) {}
+//   for (let i = 0; i < arr.length; i++) {
+//     if (
+//       arr.slice(0, i).reduce((acc, num) => acc + num, 0) ===
+//       arr.slice(i + 1).reduce((acc, num) => acc + num, 0)
+//     ) {
+//       return i
+//     }
+//   }
+//   return -1
 // }
+
 // console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])) //,3, "The array was: [1,2,3,4,3,2,1] \n");
 // console.log(findEvenIndex([1, 100, 50, -51, 1, 1])) //,1, "The array was: [1,100,50,-51,1,1] \n");
 // console.log(findEvenIndex([1, 2, 3, 4, 5, 6])) //,-1, "The array was: [1,2,3,4,5,6] \n");
@@ -84,7 +65,6 @@
 // function balance(book) {
 //   const result = book.replace(/[^a-zA-Z0-9\.\s]/g, '').split('\n')
 
-//   const resultbook.split('').filter(x => !'?:!:,'.includes(x)).join('')
 //   console.log(result)
 // }
 // // "Original_Balance:_1000.00
@@ -149,49 +129,6 @@
 // 		Test.assertEquals(balance(b2), b2sol)
 // 	})
 // })
-// //+ #135  Sort with a sorting array
-// function sort(initialArray, sortingArray) {
-//   const newArr = []
-//   for (let i = 0; i < initialArray.length; i++) {
-//     newArr[sortingArray[i]] = initialArray[i]
-//   }
-
-//   return newArr
-// }
-
-// console.log(sort([1, 2, 3, 4, 5], [0, 2, 1, 4, 3])) //, [1, 3, 2, 5, 4]);
-
-//+ #136 Zero-plentiful Array
-
-// function zeroPlentiful(arr) {
-//   // const zeros = arr.filter((zero) => zero === 0)
-//   // return zeros.length > 3 ? Math.floor(zeros.length / 4) : 0
-//   let counter = 0
-//   let zero = 0
-//   let all = 0
-
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === 0) {
-//       counter++
-//       if (counter === 1) {
-//         all++
-//       }
-//       if (counter === 4) {
-//         zero++
-//       }
-//     } else {
-//       counter = 0
-//     }
-//   }
-//   console.log({ all, zero })
-//   if (all !== zero) {
-//     return 0
-//   }
-//   return zero
-// }
-// console.log(zeroPlentiful([3])) ///,0);
-// console.log(zeroPlentiful([0, 0, 0, 0, 1, 0])) ///,1);
-// console.log(zeroPlentiful([0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0])) ///,2);0
 
 // // #137 RGB To Hex Conversion  ----
 
@@ -204,78 +141,32 @@
 // console.log(rgb(300, 255, 255)) //, 'FFFFFF')
 // console.log(rgb(173, 255, 47)) //, 'ADFF2F')
 
-//+ #138 Birthday I - Cake ????? не все тесты проходит
-//codePointAt --????
+// //+ #138 Birthday I - Cake
 // function cake(x, y) {
 //   const total = Array.from(y)
-//     .map((item, i) => (i % 2 === 0 ? y.charCodeAt(i) : i + 1))
+//     .map((item, i) => (i % 2 === 0 ? y.charCodeAt(i) : y.charCodeAt(0) - 96))
 //     .reduce((acc, num) => acc + num)
 //   console.log(total)
 //   let candles = x * 0.7
 //   return total >= candles ? 'Fire!' : 'That was close!'
 
-// //   You will work out the number of candles that will fall from the provided string (y).
-// //   You must add up the character ASCII code of each even indexed (assume a 0 based indexing) character in y, with the alphabetical position of each odd indexed character in y to give the string a total.
+//   //   You will work out the number of candles that will fall from the provided string (y).
+//   //   You must add up the character ASCII code of each even indexed (assume a 0 based indexing) character in y, with the alphabetical position of each odd indexed character in y to give the string a total.
 
-// // example: 'abc' --> a=97, b=2, c=99 --> y total = 198.
-
-//   // let total = 0
-//   // for (let i = 0; i < y.length; i++) {
-//   //   if (i % 2 === 0) {
-//   //     total += y.charCodeAt(i)
-//   //   } else {
-//   //     total += i + 1
-//   //   }
-//   // }
-//   // let candles = x * 0.7
-//   // return total >= candles ? 'Fire!' : 'That was close!'
+//   // example: 'abc' --> a=97, b=2, c=99 --> y total = 198.
 // }
 
 // console.log(cake(900, 'jmhttbhi')) //, 'That was close!');
 // console.log(cake(56, 'ifkhchlhfd')) //, 'Fire!');
 // console.log(cake(256, 'aaaaaddddr')) //, 'Fire!');
 
-//+ #139 The Span Function
-function span(arr, predicate) {
-  const one = []
-  const array = arr.every((item) => predicate(item) && one.push(item))
+//#140 Element equals its index ?????? That took too long, duration ( actual: 3331 ms ) should be under 150 ms
 
-  console.log(a)
-  const two = arr.slice(one.length)
-  return [one, two]
-
-  //   const a = arr.findIndex((item) => !predicate(item))
-  //   const one = arr.slice(0, a)
-  //   const two = arr.slice(a)
-}
-// Functions for use in tests
-function isEven(x) {
-  return Math.abs(x) % 2 === 0
-}
-
-function isOdd(x) {
-  return Math.abs(x) % 2 !== 0
-}
-
-// Arrays for use in tests
-var arr1 = [2, 4, 6, 1, 4, 8]
-var arr2 = [1, 4, 5, 7, 6]
-var arr3 = [13, 17, 19, 11, 21]
-
-//console.log(span([], tr)) //, [[], []])) //;
-
-console.log(span(arr1, isEven)) //, [[2,4,6],[1,4,8]]);
-
-console.log(span(arr2, isEven)) //, [[], arr2]);
-
-console.log(span(arr3, isOdd)) //, [arr3,[]]);
-
-// #140 Element equals its index ?????? That took too long, duration ( actual: 3331 ms ) should be under 150 ms
-
-// sorted array → binary search
+//sorted array → binary search
 function indexEqualsValue(a) {
-  const b = a.filter((item, i) => item === i)
-  return b.length > 0 ? b[0] : -1
+  const first = 0
+  const last = a.length - 1
+  let mid = Math.floor(first + last / 2)
 }
 
 console.log(indexEqualsValue([-8, 0, 2, 5])) //, 2 );
