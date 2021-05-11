c364194c4acf0681df68112cc1b21dacc19cc9f9
Anton <waytofall@inbox.ru>
2021-05-11 07:43:25 +0300

    object

--- arrays4.js
+++ arrays4.js
@@ -1,7 +1,7 @@
-// //+ #129 Sort the odd ???
+//+ #129 Sort the odd ???
 
 // function sortArray(array) {
-//   const odds = array.filter((num) => num % 2).sort((a, b) => a - b) //////// (num % 2 === 1) НЕ проходит тесты с отрицательными числами.
+//   const odds = array.filter((num) => num % 2 !== 0).sort((a, b) => a - b) //////// (num % 2 === 1) НЕ проходит тесты с отрицательными числами.
 //   console.log('...', odds)
 //   let i = 0
 //   const newArr = array.map((item) => {
@@ -22,18 +22,23 @@
 // console.log(sortArray([5, 3, 1, 8, 0])) //, [1, 3, 5, 8, 0]);
 // console.log(sortArray([])) //,[]);
 
-// //+#132 Simple Fun #79: Delete a Digit
+//+#132 Simple Fun #79: Delete a Digit
 // function deleteDigit(n) {
-//   const arr = Array.from(n.toString())
-//   let number = 0
-//   for (let i = 0; i < arr.length; i++) {
-//   let sum = +(arr.slice(0, i) + arr.slice(i + 1)).split(',').join('')
-//     if (sum > number) {
-//       number = sum
-//     }
+//   const str = n.toString()
+//   const numbers = []
+//   for (let i = 0; i < str.length; i++) {
+//     numbers.push(+(str.slice(0, i) + str.slice(i + 1)));
 //   }
-//   return number
+//   // const numbers = Array.from(str, (_, i) => str.slice(0, i) + str.slice(i + 1));
+//   return Math.max(...number);
 // }
+
+// "1234567"
+
+// [1, 2] + [3, 4] === 1234 // arr.toString() === arr.join(',')
+// "1,2" + "3,4" === "1,23,4"
+
+// const qwe = {x : 1} + {z : 4} // "[object Object][object Object]"
 
 // console.log('>>>', deleteDigit(1234567))
 // console.log(deleteDigit(152)) //,52)
@@ -43,7 +48,9 @@
 //  console.log(deleteDigit(10)) //,1)
 
 //+ #133  Equal Sides Of An Array
-// function findEvenIndex(arr) {
+// function findEvenIndex(arr) { // O(n²)
+//   let l = 0;
+//   let r = arr.reduce((acc, num) => acc + num, 0);
 //   for (let i = 0; i < arr.length; i++) {
 //     if (
 //       arr.slice(0, i).reduce((acc, num) => acc + num, 0) ===
@@ -61,41 +68,60 @@
 // console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35])) //,3, "The array was: [20,10,30,10,10,15,35] \n");
 // console.log(findEvenIndex([20, 10, 30, 10, 5, 5, 5, 5])) //,2, "The array was: [20,10,30,10,10,15,35] \n");
 
-//+ #134 Easy Balance Checking
+// //+ #134 Easy Balance Checking
 // function balance(book) {
-//   const arr = book.replace(/[^a-zA-Z0-9\.\s]/g, '').split('\n')
+//   const [firstLine, ...arr] = book
+//     .replace(/[^a-zA-Z0-9\.\s]/g, '')
+//     .split('\n')
+//     .filter((s) => s !== '')
 //   const result = []
-//   let bal = arr[0]
-//   result.push('Original Balance: ' + (+arr[0]).toFixed(2))
-//   let count = 0
-//   for (let i = 1; i < arr.length; ++i)
-//     if (arr[i].length != 0) {
-//       count++
-//       let ar = arr[i].split(' ')
-//       bal = +bal - +ar[2]
-//       result.push(
-//         `${ar[0]} ${ar[1]} ${(+ar[2]).toFixed(2)} Balance ${bal.toFixed(2)}`
-//       )
-//     }
-//   result.push('Total expense  ' + (arr[0] - bal).toFixed(2))
-//   result.push('Average expense  ' + ((arr[0] - bal) / count).toFixed(2))
+
+//   const originalBalace = Number(firstLine)
+//   let balance = originalBalace
+
+//   result.push('Original Balance: ' + originalBalace.toFixed(2))
+
+//   for (const item of arr) {
+//     const [id, title, priceStr] = item.split(' ')
+//     const price = Number(priceStr)
+//     balance -= price
+//     result.push(
+//       `${id} ${title} ${price.toFixed(2)} Balance ${balance.toFixed(2)}`
+//     )
+//   }
+
+//   const expense = originalBalace - balance
+//   const average = expense / arr.length
+
+//   result.push('Total expense  ' + expense.toFixed(2))
+//   result.push('Average expense  ' + average.toFixed(2))
+
 //   return result.join('\r\n')
 // }
 
 //+ #137 RGB To Hex Conversion
-// function rgb(r, g, b) {
-//   function hex(c) {
-//     if (c > 255) return 'FF'
-//     else if (c < 0) return '00'
-//     else {
-//       let hex = c.toString(16).toUpperCase() //else return c.toString(16).padStart(2, '0').toUpperCase()
-//       if (c < 16) {
-//         hex = '0' + hex
-//       }
-//       return hex
-//     }
+// function hex(c) {
+//   if (c > 255) {
+//     return 'FF'
 //   }
-//   return hex(r) + hex(g) + hex(b)
+//   if (c < 0) {
+//     return '00'
+//   }
+
+//   // let hex = c.toString(16).toUpperCase() //else return c.toString(16).padStart(2, '0').toUpperCase()
+//   // if (c < 16) {
+//   //   hex = '0' + hex
+//   // }
+//   // return hex
+
+//   return c.toString(16).padStart(2, '0').toUpperCase();
+// }
+
+// // https://caniuse.com/?search=padStart
+
+// function rgb(r, g, b) {
+//   return [r, g, b].map(hex).join("");
+//   // return hex(r) + hex(g) + hex(b)
 // }
 
 // console.log(rgb(0, 0, 0)) //, '000000')
@@ -104,12 +130,13 @@
 // console.log(rgb(173, 255, 47)) //, 'ADFF2F')
 
 // //+ #138 Birthday I - Cake
+// const LETTER_A_CHARCODE = 'a'.charCodeAt(0);
 // function cake(x, y) {
 //   const total = Array.from(y)
-//     .map((item, i) => (i % 2 === 0 ? y.charCodeAt(i) : y.charCodeAt(0) - 96))
-//     .reduce((acc, num) => acc + num)
+//     .map((item, i) => (i % 2 === 0 ? item.charCodeAt(0) : item.charCodeAt(0) - LETTER_A_CHARCODE + 1))
+//     .reduce((acc, num) => acc + num, 0)
 //   console.log(total)
-//   let candles = x * 0.7
+//   const candles = x * 0.7
 //   return total >= candles ? 'Fire!' : 'That was close!'
 
 //   //   You will work out the number of candles that will fall from the provided string (y).
@@ -122,7 +149,7 @@
 // console.log(cake(56, 'ifkhchlhfd')) //, 'Fire!');
 // console.log(cake(256, 'aaaaaddddr')) //, 'Fire!');
 
-//+ #140 Element equals its index ?????? That took too long, duration ( actual: 3331 ms ) should be under 150 ms
+// + #140 Element equals its index ?????? That took too long, duration ( actual: 3331 ms ) should be under 150 ms
 
 //sorted array → binary search
 // function indexEqualsValue(a) {
@@ -150,24 +177,24 @@
 //+ #141  Sort by Last Char
 
 // function last(x) {
-//   const reverse = x
-//     .split(' ')
-//     .map((item) => Array.from(item).reverse().join(''))
-//   const newReverse = reverse.sort(
-//     (a, b) => a.charCodeAt(a[0]) - b.charCodeAt(b[0])
-//   )
-//   return newReverse.map((item) => Array.from(item).reverse().join(''))
-//   //return x.split(" ").sort((a,b) => a.charCodeAt(a.length -1) - b.charCodeAt(b.length-1));
+//   // const reverse = x
+//   //   .split(' ')
+//   //   .map((item) => Array.from(item).reverse().join(''))
+//   // const newReverse = reverse.sort(
+//   //   (a, b) => a.charCodeAt(0) - b.charCodeAt(0)
+//   // )
+//   // return newReverse.map((item) => Array.from(item).reverse().join(''))
+//   return x.split(" ").sort((a,b) => a[a.length - 1] < b[b.length - 1] ? -1 : 1);
 // }
 
-// console.log(last('man i need a taxi up to ubud')) //, ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+// console.log(last('man i need a taxi0 u1 2to ubud3')) //, ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
 // console.log(last('what time are we climbing up the volcano')) //, ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
 // console.log(last('take me to semynak')) //, ['take', 'me', 'semynak', 'to']);
 
-// //+ #142 Sort array by string length
+//+ #142 Sort array by string length
 
 // function sortByLength(array) {
-//   return array.sort((a, b) => a.length - b.length)
+//   return array.sort((a, b) => (a.length < b.length ? -1 : 1))
 // }
 
 // console.log(sortByLength(['Beg', 'Life', 'I', 'To'])) //,["I", "To", "Beg", "Life"]);
@@ -176,28 +203,29 @@
 
 // console.log(sortByLength(['Longer', 'Longest', 'Short'])) //,["Short", "Longer", "Longest"]);
 
-// #143 Binary Search   ---------------------
+//#143 Binary Search   ---------------------
 
-// function binSearch(arr, toSearch) {
-//   let left = 0
-//   let right = arr.length - 1
+function binSearch(arr, toSearch) {
+  let left = 0
+  let right = arr.length - 1
 
-//   while (left < right) {
-//     let mid = Math.floor((left + right) / 2)
-//     console.log('##', arr[mid])
-//     if (arr[mid] === toSearch) {
-//       return mid
-//     } else if (arr[mid] < toSearch) {
-//       left = arr[mid]
-//     } else if (arr[mid] > toSearch) {
-//       right = arr[mid] - 1
-//     }
-//   }
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    if (arr[mid] === toSearch) {
+      return mid
+    }
+    if (arr[mid] < toSearch) {
+      left = mid + 1
+    }
+    if (arr[mid] > toSearch) {
+      right = mid - 1
+    }
+  }
 
-//   return -1
-// }
+  return -1
+}
 
-// let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5]
 
-// console.log(binSearch(arr, 6)) //, -1)
-// console.log(binSearch(arr, 2)) //, 1)
+console.log(binSearch(arr, 6)) //, -1)
+console.log(binSearch(arr, 2)) //, 1)
--- object.js
+++ object.js
@@ -46,60 +46,211 @@
 // console.log(scoreboard('The score is four nil')) //, [4,0], "Should return: [4,0]");
 // console.log(scoreboard('new score: two three')) //, [2,3], "Should return: [2,3]");
 
-// # 202 The Office I - Outed
-function outed(meet, boss) {}
+// //+ # 202 The Office I - Outed
+// function outed(meet, boss) {
+//   const len = Object.keys(meet).length
+//   let happiness = 0
+//   for (const key in meet) {
+//     if (key === boss) {
+//       happiness += meet[key] * 2
+//     } else {
+//       happiness += meet[key]
+//     }
+//   }
+// return happiness / len > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+// }
+
+// console.log(
+//   outed(
+//     {
+//       tim: 0,
+//       jim: 2,
+//       randy: 0,
+//       sandy: 7,
+//       andy: 0,
+//       katie: 5,
+//       laura: 1,
+//       saajid: 2,
+//       alex: 3,
+//       john: 2,
+//       mr: 0,
+//     },
+//     'laura'
+//   )
+// ) //, 'Get Out Now!');
+
+//+ #203 Who's Online?
+
+// const whosOnline = (friends) => {
+//   const online = []
+//   const offline = []
+//   const away = []
+
+//   friends.forEach((arr) => {
+//     if (arr.status === 'online' && arr.lastActivity < 11) {
+//       online.push(arr.username)
+//     } else if (arr.status === 'offline') {
+//       offline.push(arr.username)
+//     } else {
+//       away.push(arr.username)
+//     }
+//   })
+//   if (online.length > 0 && offline.length > 0 && away.length > 0) {
+//     return { online, offline, away }
+//   }
+
+//   if (online.length > 0 && away.length > 0) {
+//     return { online, away }
+//   }
+
+//   if (online.length > 0 && offline.length > 0) {
+//     return { online, offline }
+//   }
+//   if (offline.length > 0 && away.length > 0) {
+//     return { offline, away }
+//   }
+//   return {}
+// }
+// // const a = {
+// //   online: ['David'],
+// //   offline: ['Lucy'],
+// //   away: ['Bob'],
+// // }
+
+// const friends = [
+//   {
+//     username: 'David',
+//     status: 'online',
+//     lastActivity: 10,
+//   },
+//   {
+//     username: 'Lucy',
+//     status: 'offline',
+//     lastActivity: 22,
+//   },
+//   {
+//     username: 'Bob',
+//     status: 'online',
+//     lastActivity: 104,
+//   },
+// ]
+// console.log(whosOnline(friends))
+
+// //+ #204 Where my anagrams at?
+
+// function anagrams(word, words) {
+//   const newWords = []
+//   for (let str of words) {
+//     const newWord = word.split('').sort().join('')
+//     const newStr = str.split('').sort().join('')
+//     if (newStr === newWord) {
+//       newWords.push(str)
+//     }
+//   }
+//   return newWords
+// }
+// console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
+
+// //+  #205 Make a function that does arithmetic!
+
+// function arithmetic(a, b, operator) {
+//   switch (operator) {
+//     case 'add':
+//       return a + b
+//     case 'subtract':
+//       return a - b
+//     case 'multiply':
+//       return a * b
+//     case 'divide':
+//       return a / b
+//   }
+// }
+// console.log(arithmetic(5, 2, "multiply")) //, 10, "'multiply' should return a multiplied by b!");
+// console.log(arithmetic(8, 2, "divide")) //, 4, "'divide' should return a divided by b!");
+
+// //+ #206 Pluck
+
+// function pluck(objs, name) {
+//   const arr = []
+//   for (let i = 0; i < objs.length; i++) {
+//     arr.push(objs[i][name])
+//   }
+//   return arr
+// }
+
+// console.log(pluck([{ a: 1 }, { a: 2 }], 'a')) //, [1,2])
+// console.log(pluck([{ a: 1 }, { a: 2 }, { b: 2 }], 'a')) //, [1,2])
+
+// #207 Remove duplicate words   ------
+
+function removeDuplicateWords(s) {
+  const arr = s.split(' ')
+  const newArr = []
+  for (let i = 0; i < s.length; i++) {
+    if (arr[i + 1] === arr[i]) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr.join(' ')
+}
 
 console.log(
-  outed(
-    {
-      tim: 0,
-      jim: 2,
-      randy: 0,
-      sandy: 7,
-      andy: 0,
-      katie: 5,
-      laura: 1,
-      saajid: 2,
-      alex: 3,
-      john: 2,
-      mr: 0,
-    },
-    'laura'
+  removeDuplicateWords(
+    'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
   )
-) //, 'Get Out Now!');
-console.log(
-  outed(
-    {
-      tim: 1,
-      jim: 3,
-      randy: 9,
-      sandy: 6,
-      andy: 7,
-      katie: 6,
-      laura: 9,
-      saajid: 9,
-      alex: 9,
-      john: 9,
-      mr: 8,
-    },
-    'katie'
+) //, 'alpha beta gamma delta')
+
+// #208 Find the unique number -----------
+function findUnique(numbers) {
+  return numbers.find(
+    (item) => numbers.indexOf(item) === numbers.lastIndexOf(item)
   )
-) //, 'Nice Work Champ!');
-console.log(
-  outed(
-    {
-      tim: 2,
-      jim: 4,
-      randy: 0,
-      sandy: 5,
-      andy: 8,
-      katie: 6,
-      laura: 2,
-      saajid: 2,
-      alex: 3,
-      john: 2,
-      mr: 8,
-    },
-    'john'
-  )
-) //, 'Get Out Now!');
+}
+
+function findUnique(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i]))
+      return numbers[i]
+  }
+}
+console.log(findUnique([1, 4, 4, 5, 5, 3, 3, 2, 2])) //, 1);
+console.log(findUnique([2, 2, 5, 5, 4, 3, 3, 1, 1])) //, 4);
+console.log(findUnique([3, 5, 5, 4, 4, 3, 2, 2, 9])) //, 9);
+
+//+ #209 Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+// function greetDevelopers(list) {
+//   return list.map((item) => {
+//     return {
+//       ...item,
+//       greeting: `Hi ${item.firstName}, what do you like the most about ${item.language}?`,
+//     }
+//   })
+// }
+
+// //  #210 Convert Hash To An Array
+// function convertHashToArray(hash) {
+//   return Object.entries(hash).sort()
+// }
+// console.log(convertHashToArray({ name: 'Jeremy' })) //,[["name", "Jeremy"]]);
+// console.log(convertHashToArray({ name: 'Jeremy', age: 24 })) //,[["age", 24], ["name", "Jeremy"]]);
+// console.log(
+//   convertHashToArray({ name: 'Jeremy', age: 24, role: 'Software Engineer' })
+// ) //,[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]);
+// console.log(convertHashToArray({ product: 'CodeWars', powerLevelOver: 9000 })) //,[["powerLevelOver", 9000], ["product", "CodeWars"]]);
+// console.log(convertHashToArray({})) //,[]);
+
+// #211 Kebabize
+function kebabize(str) {
+  let newLine = ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      newLine += '-' + str[i].toLowerCase()
+    } else {
+      newLine += str[i]
+    }
+  }
+  return newLine
+}
+
+console.log(kebabize('myCamelCasedString')) //, 'my-camel-cased-string');
+console.log(kebabize('myCamelHas3Humps')) //, 'my-camel-has-humps')
