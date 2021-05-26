d9608f243716fb79d6410127cd346bfa2d1053e1
Anton <waytofall@inbox.ru>
2021-05-26 07:57:09 +0300

    obj

--- object2.js
+++ object2.js
@@ -17,7 +17,7 @@
 //+ #214 Super power frequency function  ------ Map!!!! sort!!!!!!!
 // function frequency(arr, options = {}) {
 //   const map = new Map()
-//   const { criteria = (x) => x, compareTo = (a, b) => (a < b ? -1 : a !== b) } =
+//   const { criteria = (x) => x, compareTo = (a, b) => (a < b ? -1 : 1) } =
 //     options
 //   const groupArr = arr.map(criteria)
 
@@ -45,36 +45,39 @@
 // https://github.com/tc39/proposals
 
 //+ #216 From..To..Series #2: from arr1 to arr2. Find the most same sum value of pairs  -----
-function findPair(arr1, arr2) {
-  const newArr = []
-  for (let i = 0; i < arr1.length; i++) {
-    newArr.push([arr1[i], arr2[i]])
-  }
+// function findPair(arr1, arr2) {
+//   const newArr = []
+//   for (let i = 0; i < arr1.length; i++) {
+//     newArr.push([arr1[i], arr2[i]])
+//   }
 
-  const sum = newArr.map(([a, b]) => a + b)
+//   const sum = newArr.map(([a, b]) => a + b)
 
-  const obj = {}
-  for (const num of sum) {
-    if (!(num in obj)) {
-      obj[num] = 1
-    } else {
-      obj[num]++
-    }
-  }
+//   const obj = {}
+//   for (const num of sum) {
+//     if (!(num in obj)) {
+//       obj[num] = 1
+//     } else {
+//       obj[num]++
+//     }
 
-  const values = Math.max(...Object.values(obj))
+//     // obj[num] ??= 0;
+//     // obj[num]++
+//   }
 
-  const keys = Object.keys(obj).filter((value) => obj[value] === values)
+//   const values = Math.max(...Object.values(obj))
 
-  const maxValue = Math.max(...keys)
-  const result = newArr.filter((x) => x[0] + x[1] === maxValue)
+//   const keys = Object.keys(obj).filter((value) => obj[value] === values)
 
-  return result.length >= 2 ? result : []
-}
+//   const maxValue = Math.max(...keys)
+//   const result = newArr.filter((x) => x[0] + x[1] === maxValue)
 
-console.log(findPair([1, 2, 3, 4, 5], [9, 8, 0, 0, 0])) // , [[1,9],[2,8]])
-console.log(findPair([1, 2, 3, 4, 5], [0, 0, 0, 0, 0])) // , [])
-console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],[4,2],[5,1]])
+//   return result.length >= 2 ? result : []
+// }
+
+// console.log(findPair([1, 2, 3, 4, 5], [9, 8, 0, 0, 0])) // , [[1,9],[2,8]])
+// console.log(findPair([1, 2, 3, 4, 5], [0, 0, 0, 0, 0])) // , [])
+// console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],[4,2],[5,1]])
 
 //+ #217  Evaluating prefix Polish notation
 
@@ -86,17 +89,34 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 //     '*': (a, b) => a * b,
 //     '/': (a, b) => a / b,
 //   }
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     if (fn.hasOwnProperty(arr[i])) {
-//       arr.splice(i, 3, fn[arr[i]](+arr[i + 1], +arr[i + 2]))
+//   // for (let i = arr.length - 1; i >= 0; i--) {
+//   //   if (fn.hasOwnProperty(arr[i])) {
+//   //     arr.splice(i, 3, fn[arr[i]](+arr[i + 1], +arr[i + 2]))
+//   //   }
+//   // }
+//   // return Number(arr[0])
+
+//   const numbers = []
+
+//   while (arr.length > 0) {
+//     const token = arr.pop()
+//     if (token in fn) {
+//       const x = numbers.pop()
+//       const y = numbers.pop()
+
+//       numbers.push(fn[token](x, y))
+//     } else {
+//       numbers.push(+token)
 //     }
 //   }
-//   return Number(arr[0])
+
+//   return +numbers[0]
 // }
 
-// console.log(calculate('+ 3 5')) //, 8);
+// // console.log(calculate('/ + 3 5 * 2 2')) //, 2);
+
+// console.log(calculate('- 3 5')) //, -2);
 // console.log(calculate('* + 2 2 3')) //, 12);
-// console.log(calculate('/ + 3 5 * 2 2')) //, 2);
 
 // //+ #218 Convert A Hex String To RGB
 
@@ -116,7 +136,7 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 //   const arr = []
 //   let count = 1
 //   for (let i = 0; i < str.length; i++) {
-//     if (str[i] === str[i + 1]) {
+//     if (i < str.length - 1 && str[i] === str[i + 1]) {
 //       count++
 //     } else {
 //       arr.push([count, str[i]])
@@ -129,20 +149,24 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 // console.log(runLengthEncoding('abc')) //, [[1,'a'],[1,'b'],[1,'c']]);
 // console.log(runLengthEncoding('aab')) //, [[2,'a'],[1,'b']]);
 
-// #220  Is a number prime?  ------- Random tests???
+//#220  Is a number prime?  ------- Random tests???
 
 // function isPrime(num) {
+//   // O(n) → O(sqrt(n))
 //   if (num < 2) {
 //     return false
 //   }
-//   if (num <= 3) {
-//     return true
+
+//   let count = 0
+//   for (let i = 1; i * i <= num; i++) {
+//     if (num % i === 0) {
+//       count += 2
+//     }
+//     if (i * i === num) {
+//       count -= 1
+//     }
 //   }
-//   if (num % 2 === 0 || num % 3 === 0) {
-//     return false
-//   } else {
-//     return true
-//   }
+//   return count === 2
 // }
 
 // console.log(isPrime(0)) //,  false, "0 is not prime");
@@ -151,13 +175,25 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 // console.log(isPrime(73)) //, true, "73 is prime");
 // console.log(isPrime(75)) //, false, "75 is not prime");
 
+// console.time('qwerty')
+// console.log(isPrime(2 ** 31 - 1))
+// console.timeEnd('qwerty')
+
+// 1   2   4  5  10
+// 100 50  25 20 10
+
+// 100_000_000
+
+// 20 * 5000000
+// 5000000 * 20
+// A * B === 100_000_000
+// A <= B
+// A === B → A² === 100_000_000 → A = 10_000
+
 // // //+ 221  Sum of Digits / Digital Root
 
 // function digital_root(n) {
-//   let a = String(n)
-//     .split('')
-//     .map(Number)
-//     .reduce((acc, num) => acc + num, 0)
+//   let a = n
 //   while (a > 9) {
 //     a = String(a)
 //       .split('')
@@ -167,33 +203,75 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 //   return a
 // }
 
+// function digital_root(n) {
+//   if (n <= 9) {
+//     return n
+//   }
+//   const a = String(n)
+//     .split('')
+//     .map(Number)
+//     .reduce((acc, num) => acc + num, 0)
+
+//   return digital_root(a)
+// }
+
 // console.log(digital_root(16)) //, 7 )
 // console.log(digital_root(456)) //, 6 )
 
 //+ #222  Alphabetized
 
-// function alphabetized(s) {
-//   return s
-//     .replace(/\s/g, '')
-//     .replace(/[^a-zA-Z]/g, '')
-//     .split('')
-//     .sort((a, b) =>
-//       a.toUpperCase() === b.toUpperCase()
-//         ? s.indexOf(a) - s.indexOf(b)
-//         : a.localeCompare(b)
-//     )
-//     .join('')
-// }
 
-// console.log(alphabetized('The Holy Bible')) //, 'BbeehHilloTy')
+function alphabetized(s) {
+  return s
+    .replace(/\s/g, '')
+    .replace(/[^a-zA-Z]/g, '')
+    .split('')
+    .map((item, i) => [item, i])
+    .sort((a, b) => {
+      if (a[0].toUpperCase() === b[0].toUpperCase()) {
+        return a[1] < b[1] ? -1 : 1;
+      }
+      return a[0].toUpperCase() < b[0].toUpperCase() ? -1 : 1;
+    })
+    .map((item) => item[0])
+    .join('')
+}
+// [b B b B] → [[b0], [B1], [b2], [B3]] → SORT → [[b0], [B1], [b2], [B3]] →  [b B b B]
+
+
+
+console.log(alphabetized('The Holy Bible')) //, 'BbeehHilloTy')
+console.log(alphabetized('bBbB')) //, 'bBbB')
 
 //# 223 Sort Strings by Most Contiguous Vowels -------
 
 // function sortStringsByVowels(strings) {
-//
+// }
+// function maxVowels() {
 
-//   const sortArr = strings.sort((a, b) => b.length - a.length)
-//   return sortArr
+//   const vowels = {
+//     a: 1,
+//     e: 1,
+//     i: 1,
+//     o: 1,
+//     u: 1
+//   }
+//   let max = 0
+//   let count = 0
+//   strings.forEach(letter => {
+//     if (vowels[letter.toLowerCase()]) {
+//       count++
+//     }
+//     if (count > max) {
+//       max = count
+//     } else {
+//       count = 0
+//     }
+//   })
+//   if (count > max) {
+//     max = count
+//   }
+//   return max
 // }
 
 // console.log(sortStringsByVowels(['aa', 'eee', 'oo', 'iiii'])) ///,["iiii","eee","aa","oo"]);
@@ -215,61 +293,61 @@ console.log(findPair([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])) // , [[1,5],[2,4],[3,3],
 // console.log(myLanguages({ 'C++': 50, ASM: 10, Haskell: 20 })) //, [])
 
 // #225 Sports League Table Ranking
-function computeRanks(number, games) {
-  const allGames = []
-  for (const game of games) {
-    let [TeamA, TeamB, GoalA, GoalB] = game
-    let pointA = 0
-    let pointB = 0
-    let GDa = GoalA - GoalB
-    let GDb = GoalB - GoalA
-    if (GoalA > GoalB) {
-      pointA += 2
-    } else if (GoalB > GoalA) {
-      pointB += 2
-    } else {
-      pointA++
-      pointB++
-    }
-    allGames.push([TeamA, pointA, GDa])
-    allGames.push([TeamB, pointB, GDb])
-  }
-  console.log(allGames)
-  const table = {}
+// function computeRanks(number, games) {
+//   const allGames = []
+//   for (const game of games) {
+//     let [TeamA, TeamB, GoalA, GoalB] = game
+//     let pointA = 0
+//     let pointB = 0
+//     let GDa = GoalA - GoalB
+//     let GDb = GoalB - GoalA
+//     if (GoalA > GoalB) {
+//       pointA += 2
+//     } else if (GoalB > GoalA) {
+//       pointB += 2
+//     } else {
+//       pointA++
+//       pointB++
+//     }
+//     allGames.push([TeamA, pointA, GDa])
+//     allGames.push([TeamB, pointB, GDb])
+//   }
+//   console.log(allGames)
+//   const table = {}
 
-  for (let i = 0; i < allGames.length; i++) {
-    for (let j = 1; j < allGames.length; j++) {
-      if (!(allGames[i][0] in table)) {
-        table[allGames[i][0]] =
-          allGames[
-            (allGames[i][1] + allGames[j][1], allGames[i][2] + allGames[j][2])
-          ]
-      }
-      // if (allGames[i][0] === allGames[j][0]) {
-      //   table.push([
-      //     allGames[i][0],
-      //     allGames[i][1] + allGames[j][1],
-      //     allGames[i][2] + allGames[j][2],
-      //   ])
-      // }
-    }
-  }
-  console.log(table)
-}
+//   for (let i = 0; i < allGames.length; i++) {
+//     for (let j = 1; j < allGames.length; j++) {
+//       if (!(allGames[i][0] in table)) {
+//         table[allGames[i][0]] =
+//           allGames[
+//             (allGames[i][1] + allGames[j][1], allGames[i][2] + allGames[j][2])
+//           ]
+//       }
+//       // if (allGames[i][0] === allGames[j][0]) {
+//       //   table.push([
+//       //     allGames[i][0],
+//       //     allGames[i][1] + allGames[j][1],
+//       //     allGames[i][2] + allGames[j][2],
+//       //   ])
+//       // }
+//     }
+//   }
+//   console.log(table)
+// }
 
-console.log(
-  computeRanks(6, [
-    [0, 5, 2, 2],
-    [1, 4, 0, 2],
-    [2, 3, 1, 2],
-    [1, 5, 2, 2],
-    [2, 0, 1, 1],
-    [3, 4, 1, 1],
-    [2, 5, 0, 2],
-    [3, 1, 1, 1],
-    [4, 0, 2, 0],
-  ])
-) //[4,4,6,3,1,2]);
+// console.log(
+//   computeRanks(6, [
+//     [0, 5, 2, 2],
+//     [1, 4, 0, 2],
+//     [2, 3, 1, 2],
+//     [1, 5, 2, 2],
+//     [2, 0, 1, 1],
+//     [3, 4, 1, 1],
+//     [2, 5, 0, 2],
+//     [3, 1, 1, 1],
+//     [4, 0, 2, 0],
+//   ])
+// ) //[4,4,6,3,1,2]);
 //+ #226 Double Sort
 // function dbSort(a) {
 //   const num = a.filter((item) => typeof item === 'number').sort((a, b) => a - b)
@@ -313,28 +391,28 @@ console.log(
 
 // // #229  String incrementer
 
-function incrementString(strng) {
-  const arr = Array.from(strng)
-  let num = ''
-  let str = ''
-  for (let i = 0; i < arr.length; i++) {
-    if (String(Number(arr[i])) === arr[i]) {
-      num += arr[i]
-    } else {
-      str += arr[i]
-    }
-  }
+// function incrementString(strng) {
+//   const arr = Array.from(strng)
+//   let num = ''
+//   let str = ''
+//   for (let i = 0; i < arr.length; i++) {
+//     if (String(Number(arr[i])) === arr[i]) {
+//       num += arr[i]
+//     } else {
+//       str += arr[i]
+//     }
+//   }
 
-  const result = str + (num.slice(0, 2) === '00' ? '00' + (+num + 1) : +num + 1)
-  return result
-}
+//   const result = str + (num.slice(0, 2) === '00' ? '00' + (+num + 1) : +num + 1)
+//   return result
+// }
 
-console.log(incrementString('foobar000')) //, "foobar001");
-console.log(incrementString('foo')) //, "foo1");
-console.log(incrementString('foobar001')) //, "foobar002");
-console.log(incrementString('foobar99')) //, "foobar100");
-console.log(incrementString('foobar099')) //, "foobar100");
-console.log(incrementString('')) //, "1");
+// console.log(incrementString('foobar000')) //, "foobar001");
+// console.log(incrementString('foo')) //, "foo1");
+// console.log(incrementString('foobar001')) //, "foobar002");
+// console.log(incrementString('foobar99')) //, "foobar100");
+// console.log(incrementString('foobar099')) //, "foobar100");
+// console.log(incrementString('')) //, "1");
 
 //+ #230 Maximum subarray sum
 // function maxSequence(arr) {
---
+++ object3.js
@@ -0,0 +1,206 @@
+//+ #222  Alphabetized
+
+
+// function alphabetized(s) {
+//     return s
+//       .replace(/\s/g, '')
+//       .replace(/[^a-zA-Z]/g, '')
+//       .split('')
+//       .map((item, i) => [item, i])
+//       .sort((a, b) => {
+//         if (a[0].toUpperCase() === b[0].toUpperCase()) {
+//           return a[1] < b[1] ? -1 : 1;
+//         }
+//         return a[0].toUpperCase() < b[0].toUpperCase() ? -1 : 1;
+//       })
+//       .map((item) => item[0])
+//       .join('')
+//   }
+//   // [b B b B] → [[b0], [B1], [b2], [B3]] → SORT → [[b0], [B1], [b2], [B3]] →  [b B b B]
+
+
+//   console.log(alphabetized('The Holy Bible')) //, 'BbeehHilloTy')
+//   console.log(alphabetized('bBbB')) //, 'bBbB')
+
+//+# 223 Sort Strings by Most Contiguous Vowels  
+
+// function sortStringsByVowels(strings) {
+//     return strings.sort((a, b) => maxVowels(b) - maxVowels(a));
+// }
+
+// function maxVowels(string) {
+
+//     const vowels = {
+//         a: 1,
+//         e: 1,
+//         i: 1,
+//         o: 1,
+//         u: 1
+//     }
+//     let maxVowels = 0
+//     let count = 0
+//     for (let i = 0; i < string.length; i++) {
+//         if (vowels[string[i].toLowerCase()]) {
+//             count++
+
+//             if (count > maxVowels) {
+//                 maxVowels = count
+//             }
+//         } else {
+//             count = 0
+//         }
+//     }
+//     return maxVowels
+// }
+
+// console.log(sortStringsByVowels(['aa', 'eee', 'oo', 'iiii'])) ///,["iiii","eee","aa","oo"]);
+// console.log(sortStringsByVowels(['AIBRH', '', 'YOUNG', 'GREEEN'])) /// , ["GREEEN","AIBRH","YOUNG",""]);
+
+// //+ #224 My Languages
+// function myLanguages(results) {
+//   const arr = []
+//   for (const key in results) {
+//     if (results[key] >= 60) {
+//       arr.push(key)
+//     }
+//   }
+//   return arr.sort((a, b) => results[b] - results[a])
+// }
+
+// console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 })) //, ["Ruby", "Python"])
+// console.log(myLanguages({ Hindi: 60, Greek: 71, Dutch: 93 })) //, ["Dutch", "Greek", "Hindi"])
+// console.log(myLanguages({ 'C++': 50, ASM: 10, Haskell: 20 })) //, [])
+
+// #225 Sports League Table Ranking
+// function computeRanks(number, games) {
+//   const allGames = []
+//   for (const game of games) {
+//     let [TeamA, TeamB, GoalA, GoalB] = game
+//     let pointA = 0
+//     let pointB = 0
+//     let GDa = GoalA - GoalB
+//     let GDb = GoalB - GoalA
+//     if (GoalA > GoalB) {
+//       pointA += 2
+//     } else if (GoalB > GoalA) {
+//       pointB += 2
+//     } else {
+//       pointA++
+//       pointB++
+//     }
+//     allGames.push([TeamA, pointA, GDa])
+//     allGames.push([TeamB, pointB, GDb])
+//   }
+//   console.log(allGames)
+//   const table = {}
+
+//   for (let i = 0; i < allGames.length; i++) {
+//     for (let j = 1; j < allGames.length; j++) {
+//       if (!(allGames[i][0] in table)) {
+//         table[allGames[i][0]] =
+//           allGames[
+//             (allGames[i][1] + allGames[j][1], allGames[i][2] + allGames[j][2])
+//           ]
+//       }
+//       // if (allGames[i][0] === allGames[j][0]) {
+//       //   table.push([
+//       //     allGames[i][0],
+//       //     allGames[i][1] + allGames[j][1],
+//       //     allGames[i][2] + allGames[j][2],
+//       //   ])
+//       // }
+//     }
+//   }
+//   console.log(table)
+// }
+
+// console.log(
+//   computeRanks(6, [
+//     [0, 5, 2, 2],
+//     [1, 4, 0, 2],
+//     [2, 3, 1, 2],
+//     [1, 5, 2, 2],
+//     [2, 0, 1, 1],
+//     [3, 4, 1, 1],
+//     [2, 5, 0, 2],
+//     [3, 1, 1, 1],
+//     [4, 0, 2, 0],
+//   ])
+// ) //[4,4,6,3,1,2]);
+//+ #226 Double Sort
+// function dbSort(a) {
+//   const num = a.filter((item) => typeof item === 'number').sort((a, b) => a - b)
+//   const str = a.filter((item) => typeof item === 'string').sort()
+//   return [...num, ...str]
+// }
+
+// console.log(dbSort([6, 2, 3, 4, 5])) //, [2, 3, 4, 5, 6]);
+// console.log(dbSort(['Banana', 'Orange', 'Apple', 'Mango', 0, 2, 2])) //, [0,2,2,"Apple","Banana","Mango","Orange"]);
+// console.log(dbSort(['C', 'W', 'W', 'W', 1, 2, 0])) //, [0,1,2,"C","W","W","W"]);
+
+// // #227 Sorting by bits
+// function sortByBit(arr) {
+
+// }
+
+// //+ #228 Persistent Bugger.
+// function persistence(num) {
+//     if (num < 10) {
+//         return 0
+//     }
+//     count = 0
+//     while (num > 9) {
+//         num = String(num)
+//             .split('')
+//             .map(Number)
+//             .reduce((acc, num) => acc * num)
+//         count++
+//     }
+//     return count
+// }
+// console.log(persistence(39)) //,3);
+// console.log(persistence(4)) //,0);
+// console.log(persistence(25)) //,2);
+// console.log(persistence(999)) //,4);
+
+  // // #229  String incrementer
+
+  // function incrementString(strng) {
+  //   const arr = Array.from(strng)
+  //   let num = ''
+  //   let str = ''
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (String(Number(arr[i])) === arr[i]) {
+  //       num += arr[i]
+  //     } else {
+  //       str += arr[i]
+  //     }
+  //   }
+
+  //   const result = str + (num.slice(0, 2) === '00' ? '00' + (+num + 1) : +num + 1)
+  //   return result
+  // }
+
+  // console.log(incrementString('foobar000')) //, "foobar001");
+  // console.log(incrementString('foo')) //, "foo1");
+  // console.log(incrementString('foobar001')) //, "foobar002");
+  // console.log(incrementString('foobar99')) //, "foobar100");
+  // console.log(incrementString('foobar099')) //, "foobar100");
+  // console.log(incrementString('')) //, "1");
+
+  //+ #230 Maximum subarray sum
+  // function maxSequence(arr) {
+  //   let res = 0
+
+  //   for (let i = 0; i < arr.length; i++) {
+  //     let acc = 0
+  //     for (let j = i; j < arr.length; j++) {
+  //       acc += arr[j]
+  //       if (acc > res) {
+  //         res = acc
+  //       }
+  //     }
+  //   }
+  //   return res
+  // }
+  // console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])) //, 6);
